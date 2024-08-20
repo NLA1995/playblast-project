3816e850036e533bda0ac621f199737ce90ec310
Natalia <lunanatalia95@gmail.com>
2024-08-20 11:18:49 -0600

    Requested changes to functions create_water_mark and create_black_bar

---
+++ src/ui/playblast_ui.py
@@ -0,0 +1,101 @@
+from PySide6.QtWidgets import QApplication, QWidget, QFormLayout, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, \
+    QPushButton
+
+
+class PlayblastManager(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Configure the window
+        self.setWindowTitle("Playblast Manager")
+        self.setGeometry(300, 300, 500, 250)
+
+        # Initialize the form layout
+        form_layout = QFormLayout()
+
+        # First row
+        first_row = QLabel("Name of playblast:")
+        line_edit = QLineEdit()
+        form_layout.addRow(first_row, line_edit)
+
+        # Second row
+        second_row = QLabel("Frame rate:")
+        line_edit_3 = QLineEdit()
+        form_layout.addRow(second_row, line_edit_3)
+
+        # Define the labels and corresponding QLineEdits
+        settings_labels = [
+            "Size:",
+            "Frame range:",
+        ]
+
+        # Add QLabel and two QLineEdit widgets side by side for each label
+        for label_name in settings_labels:
+            label = QLabel(label_name)
+            line_edit1 = QLineEdit()
+            line_edit2 = QLineEdit()
+
+            # Create a horizontal layout for the two QLineEdits
+            hbox_layout = QHBoxLayout()
+            hbox_layout.addWidget(line_edit1)
+            hbox_layout.addWidget(line_edit2)
+
+            # Add the label and the horizontal layout to the form layout
+            form_layout.addRow(label, hbox_layout)
+
+        # Second row
+        file_row = QLabel("File:")
+        line_edit_4 = QLineEdit()
+        form_layout.addRow(file_row, line_edit_4)
+
+        # Create the "Browse" button
+        browse_button = QPushButton("Browse")
+
+        # Create a horizontal layout to center the button
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(browse_button)
+
+        # Add the button layout to the form layout (so it appears just below the inputs)
+        form_layout.addRow(button_layout)
+
+        # Set the layout for the widget
+        self.setLayout(form_layout)
+
+        # Define the labels and corresponding QLineEdits
+        review_labels = [
+            "Artist Name:",
+            "Department:",
+            "Company Name:",
+        ]
+
+        # Add QLabel and two QLineEdit widgets side by side for each label
+        for label_name_2 in review_labels:
+            label_2 = QLabel(label_name_2)
+            line_edit3 = QLineEdit()
+
+            # Create a horizontal layout for the two QLineEdits
+            hbox_layout = QHBoxLayout()
+            hbox_layout.addWidget(line_edit3)
+
+            # Add the label and the horizontal layout to the form layout
+            form_layout.addRow(label_2, hbox_layout)
+
+        # Create the "Playblast" button
+        playblast_button = QPushButton("Playblast")
+
+        # Create a horizontal layout to center the button
+        button_2_layout = QHBoxLayout()
+        button_2_layout.addStretch()
+        button_2_layout.addWidget(playblast_button)
+
+        # Add the button layout to the form layout (so it appears just below the inputs)
+        form_layout.addRow(button_2_layout)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = PlayblastManager()
+    window.show()
+    app.exec()
+
+
--- src/utils/ffmpeg_utils.py
+++ src/utils/ffmpeg_utils.py
@@ -8,12 +8,12 @@ EXECUTABLE = join(repo_path, 'bin', 'ffmpeg', 'bin', 'ffmpeg.exe')
 
 def video_from_sequence(formatted_path, output_video, start_frame=None):
 
-    """This function creates a video based on a sequence of images.
+    """This function creates a video based on a sequence of images
 
     Args:
-        formatted_path (str): The necessary path to output the video.
-        output_video (str): The location where we want the video stored.
-        start_frame (int): The point from when we want the video to start.
+        formatted_path (str): The necessary path to output the video
+        output_video (str): The location where we want the video stored
+        start_frame (int): The point from when we want the video to start
 
     """
 
@@ -25,12 +25,28 @@ def video_from_sequence(formatted_path, output_video, start_frame=None):
     if not os.path.exists(output_video):
         raise RuntimeError(f"Failed to create video from sequence path: {formatted_path}")
 
-def create_black_bar(output_file_path):
-   run_2 = f"{EXECUTABLE} -f lavfi -i color=black:1920x80:d=3,format=rgb24 -frames:v 1 {output_file_path}"
-   os.system(run_2)
+
+def create_black_bar(output_file_path, width):
+    """ This function creates black bars in both upper and lower extremes of the video
+
+    Args:
+        output_file_path: The name of the path where the black bar will be stored
+        width: The horizontal size of the black bar
+
+    """
+    run = f"{EXECUTABLE} -f lavfi -i color=black:{width}x80:d=3,format=rgb24 -frames:v 1 {output_file_path}"
+    os.system(run)
 
 
 def create_water_mark(input_video_path, black_bar_path, output_video_path):
-   run_3 = (f"{EXECUTABLE} -i {input_video_path} -i {black_bar_path} -i {black_bar_path} "
+    """
+
+    Args:
+        input_video_path: The path of the initial video without the watermark
+        black_bar_path: The path of the black bar
+        output_video_path: The path where the video with the watermark will be stored
+
+    """
+    run = (f"{EXECUTABLE} -i {input_video_path} -i {black_bar_path} -i {black_bar_path} "
             f"-y -filter_complex [1]lut=a=val*0.1[bar1];[2]lut=a=val*0.1[bar2];[0][bar1]overlay=x=(W-w)/2:y=0[video_with_top];[video_with_top][bar2]overlay=x=(W-w)/2:y=H-h {output_video_path}")
-   os.system(run_3)
+    os.system(run)
